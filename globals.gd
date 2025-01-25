31ffb64b7ad7d3f651ab7134fadf11edbc1678fe
txrpe <txrpe@dmz.rs>
2025-01-25 19:15:08 +0100

    Add test globals timer

--- globals.gd
+++ globals.gd
@@ -1,3 +1,35 @@
 extends Node
 
 var money: int = 0
+var number_of_blocks: int = 0
+
+@onready var t = Timer.new() 
+
+signal no_more_money;
+
+func _ready() -> void:
+	add_child(t)
+	t.one_shot = false
+	t.autostart = false
+	t.wait_time = randi_range(5, 15)
+	t.timeout.connect(test)
+
+	enable_rng()
+
+func enable_rng():
+	t.start(t.wait_time)
+func disable_rng():
+	t.paused = true
+
+func test ():
+	print_debug("test")
+	
+func reduce_money(amount: int) -> bool:
+	if(money < amount):
+		no_more_money.emit()
+		return false
+	money -= amount
+	return true
+
+func increase_money(amount: int) -> void:
+	money += amount
