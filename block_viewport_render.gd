795aadf29c1a58534d5794c9bfda07becd532379
txrpe <txrpe@dmz.rs>
2025-01-26 07:45:33 +0100

    update block viewport render

---
+++ block_viewport_render.gd
@@ -0,0 +1,17 @@
+extends SubViewportContainer
+
+@export var rendered_node: PackedScene = preload("res://blocks/nadog_1.tscn")
+@onready var viewPort = $SubViewport
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if(rendered_node.can_instantiate()):
+		var n = rendered_node.instantiate()
+		$SubViewport.add_child(n)
+		n.freeze = true;
+		
+		
+		
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
