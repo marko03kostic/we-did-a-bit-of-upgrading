142cb85c8522278d7db0e5541f66299af9145df3
txrpe <txrpe@dmz.rs>
2025-01-26 02:35:57 +0100

    add dialog

--- globals.gd
+++ globals.gd
@@ -3,30 +3,29 @@ extends Node
 var money: int = 0
 var number_of_blocks: int = 0
 
-@onready var t = Timer.new() 
+@onready var earthquake_timer = Timer.new() 
 
 signal no_more_money;
 var ICON = preload("res://icon.svg")
 func _ready() -> void:
 	MainGui.visible = false;
 	
-	add_child(t)
-	t.one_shot = false
-	t.autostart = false
-	t.wait_time = randi_range(5, 6)
-	t.timeout.connect(apply_shake)
+	add_child(earthquake_timer)
+	earthquake_timer.one_shot = false
+	earthquake_timer.autostart = false
+	earthquake_timer.wait_time = randi_range(5, 6)
+	earthquake_timer.timeout.connect(apply_shake)
 	
 func start_a_level():
-	#var main_gui = get_tree().get_first_node_in_group("MainGUI") as MainGUI
 	MainGui.visible = true;
 	print_debug(ICON)
 	MainGui.set_blocks(1,  ICON, 2, ICON, 3, ICON)
-func enable_rng():
 	
-	t.start(t.wait_time)
+func enable_rng():
+	earthquake_timer.start(earthquake_timer.wait_time)
 	
 func disable_rng():
-	t.paused = true
+	earthquake_timer.paused = true
 
 func apply_shake():
 	get_tree().get_nodes_in_group("Level").all(func (lvl): lvl.apply_shake())
